2ba3142f7ad1ab5eba7ae0db6f1a7586b99dfe52
Aditya <adityachotaliya9299@gmail.com>
2026-05-03 10:19:38 +0000

    chore: export liquidate module

--- programs/shieldfi/src/instructions/mod.rs
+++ programs/shieldfi/src/instructions/mod.rs
@@ -3,9 +3,11 @@ pub mod deposit;
 pub mod withdraw;
 pub mod borrow;
 pub mod repay;
+pub mod liquidate;
 
 pub use initialize_pool::*;
 pub use deposit::*;
 pub use withdraw::*;
 pub use borrow::*;
 pub use repay::*;
+pub use liquidate::*;
