5fb3f90b307a96317b426c98212f4563cd496f11
Aditya <adityachotaliya9299@gmail.com>
2026-06-10 11:03:07 +0000

    feat(phase-1): export all state including SLOTS_PER_YEAR

--- programs/shieldfi/src/state/mod.rs
+++ programs/shieldfi/src/state/mod.rs
@@ -1,7 +1,7 @@
+pub mod oracle;
 pub mod pool;
 pub mod user_position;
-pub mod oracle;
 
-pub use pool::{LendingPool, PoolConfig, SLOTS_PER_YEAR};
-pub use user_position::*;
 pub use oracle::*;
+pub use pool::*;
+pub use user_position::*;
